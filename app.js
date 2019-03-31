0bcaf51f0a01a68ec4168028662b3f977ed860bd
Eventizy <avinash@eventizy.in>
2019-03-31 20:02:53 +0530

    second

--- app.js
+++ app.js
@@ -5,7 +5,6 @@ const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const expressValidator = require('express-validator');
 const indexRouter = require('./routes/index');
-const usersRouter = require('./routes/users');
 const MongoClient = require('mongodb').MongoClient;
 const app = express();
 const mongoose = require('mongoose');
@@ -31,7 +30,6 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
